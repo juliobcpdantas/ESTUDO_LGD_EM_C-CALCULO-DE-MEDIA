b34c59aa4a734399efd38d4a9b760ca3ad221264
JULIOBCPDANTAS <85968604+juliobcpdantas@users.noreply.github.com>
2021-06-21 10:42:28 -0300

    segundo código - Equação de segundo grau
    Estudos de Lógica de programação em C++, Equação de segundo grau com comentários de estrutura do código para fins didáticos.

---
+++ EQ2GRAU.cpp
@@ -0,0 +1,13 @@
+ #include <iostream> 
+#include <cstdlib>
+#include <cmath> //informe de bibliotecas utilizadas
+using namespace std;
+
+int main(int argc, char** argv){ //definição de argumentos, numeros inteiros e uso de caractéres
+	float x1,x2; //declaração de variáveis flutuantes
+	float a,b,c;
+	float delta;
+	
+}
+
+
